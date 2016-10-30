0a074a92e28c86c00c5e17639c04b0bd6f9eb443
Kots Olesya <kots.olesya@gmail.com>
2016-10-30 13:10:42 +0200

    Tamagochy v1.0 beta bird add menu and help

--- birds.rb
+++ birds.rb
@@ -1,101 +1,134 @@
 require 'date'
 require 'timers'
 
-
 class Bird
-    attr_accessor :name, :type
+  attr_accessor :name, :type
 
+  def initialize
+    @name = nil
+    @type = 1
+    @health = 100
+    @birthday = nil
+    @energy = 100 # golod == 0
+    @angry = 1
+    @live = false
+    @birds_type = ['Red Bird', 'Blue Birds',
+                   'Yellow Bird ', 'Black Bird ',
+                   'White Bird', 'Boomerang Bird',
+                   'Big Brother Bird', 'Orange Bird',
+                   'Pink Bird', 'Grey Bird']
+  end
 
-
-    def initialize()
-      @name = nil
-      @type = 1
-      @health = 100
-      @birthday = nil
-      @energy = 100 #голод == 0, ситий 100
-      @angry = 1
-      @life = false
-
-      @birds_type = ["Red Bird" , "Blue Birds" ,
-                    "Yellow Bird ", "Black Bird ",
-                    "White Bird", "Boomerang Bird",
-                    "Big Brother Bird", "Orange Bird",
-                    "Pink Bird", "Grey Bird"]
-
+  def main_work
+    puts 'Please, select your choice'
+    case gets.to_i
+    when 1 then bird_born
+    when 2 then show_bird_status if alive?
+    when 3 then bird_age if alive?
+    when 4 then feed if alive?
+    when 5 then fly if alive?
+    when 6 then needs if alive?
+    when 0 then exit
+    else puts "I don't know what to do"
     end
+  end
 
-    def timer(n)
-      timers = Timers::Group.new
-      now_and_every_one_seconds = timers.now_and_every(1) { print "." }
-      n.times { timers.wait }
-    end
+  def help
+    puts 'Tamagochi v1.0 Beta "Birds"'
+    puts '------------Menu-------------'
+    puts '1 - create your bird'
+    puts "2 - show bird's status"
+    puts "3 - show bird's age"
+    puts '4 - feed your bird'
+    puts '5 - let the bird flight'
+    puts '6 - needs'
+    puts "0 - exit"
+  end
 
-    def bird_born
+  def bird_born
+    if alive?
+      puts 'Bird have allready born'
+    else
       puts "Bird's name:"
-      @name = gets
+      @name = gets.chomp
       puts "Bird's type (number):"
       t = gets
       @type = t.to_i
       @birthday = Time.now.hour
-      @life = true
-       #@birds_type[@type]
-       timer(5)
-      puts ' '
-      puts "Bird #{@name} just born"
-    end
-
-    def bird_age
-      Time.now.hour - @birthday
-    end
-
-    def show_bird_status
-      if @life
-      puts "Bird's name #{@name}"
-      puts "Bird's type #{@birds_type[@type]}"
-      puts "Bird's health #{@health}"
-      puts "Bird's birthday #{bird_age.to_i}"
-      puts "Bird's energy #{@energy}"
-      puts "Bird's angry #{@angry}"
-      puts "Bird's poo #{@poo}"
-      puts "-----------------------------------------------"
-      puts "#{@name} wants eat!  (#{@energy}%)" if hungry?
-      puts "#{@name} eats a lot and becomes a fatty, so can not fly  (#{@energy}%)" if overfed?
-      puts "-----------------------------------------------"
-
-      else
-        puts "Bird was not born yet, please call method bird_born"
-      end
-    end
-
-    def feed # їсть щоб отримати енергію
-        @energy +=33
-        puts "#{@name} eat..."
+      @live = true
       timer(5)
-      puts("thank you")
+      puts "\nBird #{@name} just born"
     end
+  end
 
-    def fly # літає щоб потратити енергію
-      @energy -=10
+  def bird_age
+    puts Time.now.hour - @birthday
+  end
+
+  def show_bird_status
+    puts "Bird's name #{@name}"
+    puts "Bird's type #{@birds_type[@type]}"
+    puts "Bird's health #{@health}"
+    puts "Bird's birthday #{bird_age.to_i}"
+    puts "Bird's energy #{@energy}"
+    puts "Bird's angry #{@angry}"
+  end
+
+  def needs
+    puts "#{@name} wants eat!  (#{@energy}%)" if hungry?
+    puts "#{@name} eats a lot and becomes a fatty, so can not fly  (#{@energy}%)" if overfed?
+  end
+
+  def feed
+    if overfed?
+      puts "#{@name} can not eat anymore, it is fat;)"
+    else
+      @energy += 33
+      puts "#{@name} eating..."
+      timer(5)
+      puts('thank you')
+    end
+  end
+
+  def fly # fly to expend energy
+    if hungry?
+      puts "#{@name} can not fly, it is hungry;("
+    else
+      @energy -= 10
       puts "#{@name} flies..."
       timer(5)
-      puts("pleased")
+      puts('pleased')
     end
+  end
 
-    def hungry?  # чи голодна пташка
-      @energy <= 5
+  private
+
+  def hungry?
+    @energy <= 5
+  end
+
+  def overfed? # eats to much
+    @energy >= 100
+  end
+
+  def alive?
+    if @live
+      true
+    else
+      puts 'bird not a live'
+      false
     end
+  end
 
-
-    def overfed?  # чи не переїла
-      @energy >= 100
-    end
-
-
+  def timer(n)
+    timers = Timers::Group.new
+    timers.now_and_every(1) { print '.' }
+    n.times { timers.wait }
+  end
 end
 
-  b = Bird.new
-  b.bird_born
-  b.show_bird_status
-b.feed
-b.fly
-b.show_bird_status
+b = Bird.new
+b.help
+loop do
+  b.main_work
+end
